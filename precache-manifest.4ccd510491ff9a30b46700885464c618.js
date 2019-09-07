5d2962df5bb3c1bcaf5c356a7e96331ae0febf3c
Joji Mathew <jojimathew.mec@gmail.com>
2019-09-07 23:48:35 +0530

    Updates

---
+++ precache-manifest.4ccd510491ff9a30b46700885464c618.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d3477458ccc5dc253a19f856d96bc97",
+    "url": "/star-rating-component/index.html"
+  },
+  {
+    "revision": "7f81779d2d69a5a7b232",
+    "url": "/star-rating-component/static/css/main.2b9c2137.chunk.css"
+  },
+  {
+    "revision": "c61a771922bb06608be8",
+    "url": "/star-rating-component/static/js/2.64d4731d.chunk.js"
+  },
+  {
+    "revision": "7f81779d2d69a5a7b232",
+    "url": "/star-rating-component/static/js/main.26d6388f.chunk.js"
+  },
+  {
+    "revision": "22b27d8bf5c29343bac0",
+    "url": "/star-rating-component/static/js/runtime~main.d3ce90bd.js"
+  }
+]);
